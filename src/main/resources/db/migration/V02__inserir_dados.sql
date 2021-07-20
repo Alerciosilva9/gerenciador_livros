2027c1f89725c9fb7a5f219ac544749995addbc0
Alercio Silva <silvaalercio@gmail.com>
2021-07-20 18:42:17 -0300

    migration v6

--- src/main/resources/db/migration/V02__inserir_dados.sql
+++ src/main/resources/db/migration/V02__inserir_dados.sql
@@ -1,3 +1,3 @@
 insert into Autor(id,nome,idade) values (1,'carlos',12);
 insert into Autor(id,nome,idade) values (2,'marlon',12);
-insert into Autor(nome,idade) values ('carlos',12);
+insert into Autor(nome,idade) values (3,'carlos',12);
