1017181f4ec0f3ebb6af06077ee788b751f9b637
Alercio Silva <silvaalercio@gmail.com>
2021-07-20 16:36:39 -0300

    corrigindo erros migracao

--- src/main/resources/db/migration/V01__criar_tabela.sql
+++ src/main/resources/db/migration/V01__criar_tabela.sql
@@ -2,7 +2,7 @@
 CREATE SEQUENCE Autor_seq;
 
 CREATE TABLE Autor(
-	id_autor BIGINT PRIMARY KEY DEFAULT NEXTVAL ('Autor_seq'),
+	id BIGINT PRIMARY KEY DEFAULT NEXTVAL ('Autor_seq'),
     nome VARCHAR(50) NOT NULL,
    	idade BIGINT NOT NULL
 ) ; 
@@ -17,11 +17,13 @@ CREATE TABLE Livro(
     capitulos  BIGINT NOT NULL,
     isbn VARCHAR(50) NOT NULL,
     nomeeditora VARCHAR(50) NOT NULL,
-    id_autor BIGINT NOT NULL,
-     FOREIGN KEY (id_autor) references Autor(id_autor)
+    id_autor BIGINT NOT NULL
 ) ; 
 
-insert into Autor(id_autor,nome,idade) values (1,'carlos',12);
+ALTER TABLE course
+  ADD CONSTRAINT fk_livro_autor FOREIGN KEY (autor_id) REFERENCES Autor;
+
+insert into Autor(id,nome,idade) values (1,'carlos',12);
 
 
 
