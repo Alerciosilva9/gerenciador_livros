d3ef118c2fb65ede5c4ec14c5afb25eebcc35dbe
Alercio Silva <silvaalercio@gmail.com>
2021-07-21 14:16:08 -0300

    adicionando classe livroservice e corrigindo o problema de id inexistente no cadastro

--- src/main/java/com/loja/gerenciadorlivros/service/LivroService.java
+++ src/main/java/com/loja/gerenciadorlivros/service/LivroService.java
@@ -1,9 +1,26 @@
 package com.loja.gerenciadorlivros.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.loja.gerenciadorlivros.entity.Livro;
+import com.loja.gerenciadorlivros.repository.AutorRepository;
+import com.loja.gerenciadorlivros.repository.LivroRepository;
 @Service
 public class LivroService {
 
-	//*
+	@Autowired
+	LivroRepository livrorepository;
+	
+	@Autowired
+	AutorRepository autorrepository;
+
+	public Livro cadastrarlivro(Livro novolivro) {
+		if(autorrepository.findById(novolivro.getAutor().getId()).orElse(null)==null) {
+			return null;
+		}else{
+			return livrorepository.save(novolivro);
+		}
+	}
 	
 }
